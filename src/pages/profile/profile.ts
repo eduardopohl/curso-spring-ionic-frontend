a6e2cb223f4cef1be55bdd2e5cbc79fcecc8c58f
Eduardo Pohl <eduardo.pohl@gmail.com>
2020-04-15 09:58:01 -0300

    Buscando enderecos do banco de dados

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -26,7 +26,7 @@ export class ProfilePage {
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
-          this.cliente = response;
+          this.cliente = response as ClienteDTO;
           this.getImageIfExists();
       },
       error => {
@@ -47,5 +47,4 @@ export class ProfilePage {
     },
     error => {});
   }
-
 }
